cd98117def0044b183f6029cf9765c0d8d22f63a
pjonnathan <jonnathanmatos856@gmail.com>
2025-01-23 22:25:57 -0300

    first commit

---
+++ src/main/java/com/example/email_service/core/EmailRequest.java
@@ -0,0 +1,4 @@
+package com.example.email_service.core;
+
+public record EmailRequest(String to, String subject, String body) {
+}
---
+++ src/main/java/com/example/email_service/core/exception/EmailServiceException.java
@@ -0,0 +1,11 @@
+package com.example.email_service.core.exception;
+
+public class EmailServiceException extends RuntimeException{
+    public EmailServiceException(String message){
+        super(message);
+    }
+
+    public EmailServiceException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
